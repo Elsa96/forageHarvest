871796cf53cee5f313da3e37a054cec5cbd88fc6
Elsa96 <zhangaisha1@hotmail.com>
2020-07-24 20:51:01 +0800

    update ZED SDK from 2.8 to 3.2

--- Cam.cpp
+++ Cam.cpp
@@ -8,12 +8,12 @@ using namespace sl;
 
 int Cam::cameraStart() {
     // Set configuration parameters
-    init_parameters.camera_resolution = RESOLUTION_HD720; // Use HD720 video mode
+    init_parameters.camera_resolution = RESOLUTION::HD720; // Use HD720 video mode
     init_parameters.camera_fps = 15; // Set fps at 15
-    init_parameters.depth_mode = DEPTH_MODE_PERFORMANCE; // Use performance depth mode
-    init_parameters.coordinate_units = UNIT_MILLIMETER; // Set units in millimeter
+    init_parameters.depth_mode = DEPTH_MODE::PERFORMANCE; // Use performance depth mode
+    init_parameters.coordinate_units = UNIT::MILLIMETER; // Set units in millimeter
     init_parameters.coordinate_system =
-            COORDINATE_SYSTEM_RIGHT_HANDED_Y_UP; // OpenGL's coordinate system is right_handed
+            COORDINATE_SYSTEM::RIGHT_HANDED_Y_UP; // OpenGL's coordinate system is right_handed
 
     // Open the camera
     ERROR_CODE err = zed.open(init_parameters);
@@ -24,7 +24,7 @@ int Cam::cameraStart() {
     }
 
     // Set runtime parameters after opening the camera
-    runtime_parameters.sensing_mode = SENSING_MODE_STANDARD; // Use STANDARD sensing mode
+    runtime_parameters.sensing_mode = SENSING_MODE::STANDARD; // Use STANDARD sensing mode
 }
 
 void Cam::cameraClose() {
@@ -35,28 +35,28 @@ cv::Mat Cam::slMat2cvMat(sl::Mat &input) {
     // Mapping between MAT_TYPE and CV_TYPE
     int cv_type = -1;
     switch (input.getDataType()) {
-        case sl::MAT_TYPE_32F_C1:
+        case sl::MAT_TYPE::F32_C1:
             cv_type = CV_32FC1; // 5
             break;
-        case sl::MAT_TYPE_32F_C2:
+        case sl::MAT_TYPE::F32_C2:
             cv_type = CV_32FC2; // 13
             break;
-        case sl::MAT_TYPE_32F_C3:
+        case sl::MAT_TYPE::F32_C3:
             cv_type = CV_32FC3; // 21
             break;
-        case sl::MAT_TYPE_32F_C4:
+        case sl::MAT_TYPE::F32_C4:
             cv_type = CV_32FC4; // 29
             break;
-        case sl::MAT_TYPE_8U_C1:
+        case sl::MAT_TYPE::U8_C1:
             cv_type = CV_8UC1; // 0
             break;
-        case sl::MAT_TYPE_8U_C2:
+        case sl::MAT_TYPE::U8_C2:
             cv_type = CV_8UC2; // 8
             break;
-        case sl::MAT_TYPE_8U_C3:
+        case sl::MAT_TYPE::U8_C3:
             cv_type = CV_8UC3; // 16
             break;
-        case sl::MAT_TYPE_8U_C4:
+        case sl::MAT_TYPE::U8_C4:
             cv_type = CV_8UC4; // 24
             break;
         default:
@@ -65,7 +65,7 @@ cv::Mat Cam::slMat2cvMat(sl::Mat &input) {
     // Since cv::Mat data requires a uchar* pointer, we get the uchar1 pointer from sl::Mat (getPtr<T>())
     // * cv::Mat and sl::Mat will share a single memory structure
     // getPtr() // Returns the CPU or GPU data pointer
-    return cv::Mat(input.getHeight(), input.getWidth(), cv_type, input.getPtr<sl::uchar1>(sl::MEM_CPU));
+    return cv::Mat(input.getHeight(), input.getWidth(), cv_type, input.getPtr<sl::uchar1>(sl::MEM::CPU));
 }
 
 cv::Mat Cam::getImage(int key) {
@@ -73,11 +73,11 @@ cv::Mat Cam::getImage(int key) {
     // TODO 这里一直运行吗？还是到main()里面一直运行
     if (zed.grab(runtime_parameters) == ERROR_CODE::SUCCESS) {
         // Retrieve left image
-        zed.retrieveImage(color, VIEW_LEFT);
+        zed.retrieveImage(color, VIEW::LEFT);
         // Retrieve depth map. Depth is aligned on the left image
-        zed.retrieveMeasure(depth, MEASURE_DEPTH);
+        zed.retrieveMeasure(depth, MEASURE::DEPTH);
         // Retrieve colored point cloud. Point cloud is aligned on the left image.
-        zed.retrieveMeasure(pointCloud, MEASURE_XYZRGBA);
+        zed.retrieveMeasure(pointCloud, MEASURE::XYZRGBA);
     }
     if (key == 0)
         return slMat2cvMat(color);
