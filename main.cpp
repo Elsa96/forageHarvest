fa87c46a11986bd495454b62890cbbb2edea6b4e
Elsa96 <zhangaisha1@hotmail.com>
2020-07-25 15:22:50 +0800

    add dst image

--- main.cpp
+++ main.cpp
@@ -14,7 +14,6 @@ int main(int argc, char *argv[]) {
             break;
         cv::Mat image = zedCamera.getImage(0); //获取彩图
         imshow("原始图像", image);
-        waitKey(10);
 
         Detection detection(image); //检测
         detection.process();
@@ -22,6 +21,7 @@ int main(int argc, char *argv[]) {
             cout << "no lines" << endl;
             continue;
         }
+        imshow("目标图像", image);
 
         // Point2i
         vector<Point2f> keyPoints2D; //获取角点+落点的像素坐标
@@ -39,11 +39,6 @@ int main(int argc, char *argv[]) {
         //        handle.setKeyPoints(edgePoints2D, edgePoints3D);
         handle.process();
     }
-
-    /*    namedWindow("原始图像", WINDOW_NORMAL);
-    resizeWindow("原始图像", 1000, 1000);
-    imshow("原始图像", image);
-    waitKey(0);*/
 
 #elif 0 // color 测试
     Mat image = imread("../images/yellowBorder6.jpg");
@@ -92,6 +87,6 @@ int main(int argc, char *argv[]) {
     waitKey(0);
 
 #endif
-    zedCamera.cameraClose();
+//    zedCamera.cameraClose();
     return 0;
 }
